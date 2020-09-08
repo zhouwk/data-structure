971b3b4f70a2052e90ffeaa926dd58bb305a9f28
zhouwk <2963329733@qq.com>
2020-09-08 11:01:32 +0800

    rename  api

--- 二叉树/main.swift
+++ 二叉树/main.swift
@@ -28,40 +28,38 @@ func createBiTree(using values: [String]) -> Node? {
     }
     let root = Node(values[0])
     var index = 0
-    createNodeChildren(root: root) { () -> Node? in
+    createChildren(for: root) { () -> Node? in
         // 该闭包的功能：累加index，遍历values
-        // 如果index == values.count，则遍历结束，则不会创建新节点
-        // 如果values[index] == EOF，则表明正在遍历的节点没有对应位置的子节点，至于具体位置，则取决于createNodeChildren中是left调用的，还是right调用的
-        index += 1
-        if index >= values.count {
-            return nil
-        }
-        let value = values[index]
-        if value == EOT {
-            return nil
-        }
-        return Node(value)
+       // 如果index == values.count，则遍历结束，则不会创建新节点
+       // 如果values[index] == EOF，则表明正在遍历的节点没有对应位置的子节点，至于具体位置，则取决于createNodeChildren中是left调用的，还是right调用的
+       index += 1
+       if index >= values.count {
+           return nil
+       }
+       let value = values[index]
+       if value == EOT {
+           return nil
+       }
+       return Node(value)
     }
     return root
 }
 
 /// 创建节点的children
-func createNodeChildren(root: Node, next: () -> Node?) {
+///
+/// - Parameter node: 当前正在遍历的节点，也是为该节点创建左右子节点
+/// - Parameter next: 闭包调用接收新的节点，新的节点将作为node的左或右子节点
+func createChildren(for node: Node, next: () -> Node?) {
     if let lChild = next() {
         root.lChild = lChild
-        createNodeChildren(root: lChild, next: next)
+        createChildren(for: node, next: next)
     }
     if let rChild = next() {
         root.rChild = rChild
-        createNodeChildren(root: rChild, next: next)
+        createChildren(for: node, next: next)
     }
 }
 
-//createBiTree(root: tempRoot)
-
-
-
-
 if let root = createBiTree(using: treeNodes) {
-    print(root.rChild?.rChild?.rChild?.lChild?.rChild?.value)
+    print(root.value)
 }
